fcd19d529b1b09525f2480a16a0521a4b27c575d
DattaDhebe <dattadhebe75@gmail.com>
2020-06-03 14:32:48 +0530

    Added : Choice Using Switch to Perform Banking Operations

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Bank
 {
@@ -6,7 +8,71 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userInput;
+            int choice = 0, amount, accountNumber;
+            Console.WriteLine("Welcome TO State Bank..!!");
+            Console.Write("1. Withdrow\n2. Deposit\n3. Check Balance\n4.Add User\n");
+            Console.WriteLine("\nSelect Your Choice : ");
+            userInput = Console.ReadLine();
+            choice = Convert.ToInt32(userInput);
+            Console.WriteLine(choice);
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter Amount To Be Withdrow : ");
+                    userInput = Console.ReadLine();
+                    amount = Convert.ToInt32(userInput);
+                    withdrow(amount);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Amount To Be Deposit : ");
+                    userInput = Console.ReadLine();
+                    amount = Convert.ToInt32(userInput);
+                    Deposit(amount);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter Account Number To Search : ");
+                    userInput = Console.ReadLine();
+                    accountNumber = Convert.ToInt32(userInput);
+                    CheckBalance(accountNumber);
+                    break;
+                case 4: 
+                    AddUser(userInput);
+                    break;
+            }
+
+
+        }
+
+        private static void AddUser(string userInput)
+        {
+            string userName;
+            int amount;
+            Console.WriteLine("Enter User Name : ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter Opening Amount : ");
+            userInput = Console.ReadLine();
+            amount = Convert.ToInt32(userInput);
+
+            BankingOperation list = new BankingOperation();
+            list.add();
+
+        }
+
+        private static void CheckBalance(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Deposit(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void withdrow(int amount)
+        {
+            throw new NotImplementedException();
         }
     }
 }
