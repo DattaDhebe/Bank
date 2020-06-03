c826c1a965cf5096712a44ee44e5f89b2b036f18
DattaDhebe <dattadhebe75@gmail.com>
2020-06-03 23:45:26 +0530

    Refactored : Code to Add Continues Details of Customer.

---
+++ Bank/BankingCashCounter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Bank
+{
+    class BankingCashCounter
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int choice = 0, amount, accountNumber;
+            while (true)
+            {
+                Console.WriteLine("Welcome TO State Bank..!!");
+                Console.Write("\n\n1. Withdrow\n2. Deposit\n3. Check Balance\n4. Add User\n5. Exit\n");
+                Console.WriteLine("\nSelect Your Choice : ");
+                userInput = Console.ReadLine();
+                choice = Convert.ToInt32(userInput);
+                Console.WriteLine(choice);
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter Amount To Be Withdrow : ");
+                        userInput = Console.ReadLine();
+                        amount = Convert.ToInt32(userInput);
+                        withdrow(amount);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter Amount To Be Deposit : ");
+                        userInput = Console.ReadLine();
+                        amount = Convert.ToInt32(userInput);
+                        Deposit(amount);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter Account Number To Search : ");
+                        userInput = Console.ReadLine();
+                        accountNumber = Convert.ToInt32(userInput);
+                        CheckBalance(accountNumber);
+                        break;
+
+                    case 4:
+                        AddUser();
+                        break;
+
+                    case 5:
+                        break;
+
+                    default:
+                        Console.WriteLine("Please Select Right Choice..");
+                        break;
+                        
+                }
+                if (choice == 5)
+                {
+                    Console.WriteLine("Thank You For Banking With Us..!!");
+                    break;
+                }
+            }
+
+
+        }
+
+        private static void AddUser()
+        {
+            string userName, userInput;
+            int amount;
+            Console.WriteLine("Enter User Name : ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter Opening Amount : ");
+            userInput = Console.ReadLine();
+            amount = Convert.ToInt32(userInput);
+
+            LinkList<UserRegistration> list = new LinkList<UserRegistration>();
+            object userDetails = userName + "\n" + amount;
+            list.Add(userDetails);
+            list.PrintList();
+            
+        }
+
+        private static void CheckBalance(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Deposit(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void withdrow(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Bank/LinkList.cs
+++ Bank/LinkList.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Drawing;
 
 namespace Bank
 {
     public class LinkList<T>
     {
+        int count = 0;
         public Node head; // head of list 
 
         /* Linked list Node*/
@@ -11,33 +13,27 @@ namespace Bank
         {
             
             public Node next;
-            public string data;
-            public UserRegistration userRegistration;
-
-            public Node(UserRegistration userRegistration)
+            public object userDetails;
+            
+            public Node(object userDetails)
             {
-                this.userRegistration = userRegistration;
+                this.userDetails = userDetails;
+                next = null;
             }
-
-            public Node(string userRegistration) 
-            { 
-                data = userRegistration; 
-                next = null; 
-            }
-
         }
 
-        public void Add(UserRegistration userRegistration)
+        public void Add(object userDetails)
         {
             /* 1 & 2: Allocate the Node & 
                     Put in the data*/
-            Node new_node = new Node(userRegistration);
+            Node new_node = new Node(userDetails);
 
             /* 3. Make next of new Node as head */
             new_node.next = head;
 
             /* 4. Move the head to point to new Node */
             head = new_node;
+            count++;
         }
 
         public void PrintList()
@@ -45,7 +41,7 @@ namespace Bank
             Node firstNode = head;
             while (firstNode != null)
             {
-                Console.Write(firstNode.data + " ");
+                Console.Write(firstNode.userDetails + " ");
                 firstNode = firstNode.next;
             }
         }
